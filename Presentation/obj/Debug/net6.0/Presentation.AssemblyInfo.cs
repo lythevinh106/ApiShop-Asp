d36d8d1a25d90f133c4177d9c91f44377c91d0d0
thangkhovipro <thangkhovipro@gmail.com>
2024-03-04 18:46:00 +0700

    changeProduction

--- Presentation/obj/Debug/net6.0/Presentation.AssemblyInfo.cs
+++ Presentation/obj/Debug/net6.0/Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+491caab71a303935c0a4cc9842238c38182b2b89")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
